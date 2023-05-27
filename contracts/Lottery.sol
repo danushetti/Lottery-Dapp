19da02dae72388e37a52e92dcdf6b80918cb09c6
Daneshwari C Pattanashetti <pattanashettidanu7@gmail.com>
2023-05-27 16:33:58 +0530

    deployed contract

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,207 +1,151 @@
-// SPDX-License-Identifier: MIT
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
 
-pragma solidity ^0.8.7;
-
-// npm i -g hardhat-shorthand: to run shor commands in harhdat
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
-import "@chainlink/contracts/src/v0.8/interfaces/AutomationCompatibleInterface.sol";
-import "hardhat/console.sol";
 
-/* Errors */
-error Raffle__UpkeepNotNeeded(uint256 currentBalance, uint256 numPlayers, uint256 raffleState);
-error Raffle__TransferFailed();
-error Raffle__SendMoreToEnterRaffle();
-error Raffle__RaffleNotOpen();
+//contract_Address = 0xe3B2B7a470cC4b33036Bbe5fd38afa9eEed2C92E
+//configrations for vrf system of mumbai teest net
+//     vrfcoordinator - 0x7a1BaC17Ccc5b313516C5E16fb24f7659aA5ebed
+//     //subscription - 4528
+//     //keyhash- 0x4b09e658ed251bcafeebbc69400383d49f344ace09b9576fe248bb02c003fe9f
+//     //interval - 120
+//     //entranceFee - 100
+//     //callbackGasLimit - 2,500,000
 
-/**@title A sample Raffle Contract
- * @notice This contract is for creating a sample raffle contract
- * @dev This implements the Chainlink VRF Version 2
- */
- 
-contract Raffle is VRFConsumerBaseV2, AutomationCompatibleInterface {
-    /* Type declarations */
-    enum RaffleState {
-        OPEN,
-        CALCULATING
+contract Lottery is VRFConsumerBaseV2 {
+    address public manager;
+    address payable[] private participants;
+    uint public lotteryId;
+    mapping(uint => address payable) public lotteryHistory;
+
+    enum State {
+        STARTED,
+        ENDED
     }
-    
-    
-    /* State variables */
-    // Chainlink VRF Variables
-    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
-    uint64 private immutable i_subscriptionId;
-    bytes32 private immutable i_gasLane;
-    uint32 private immutable i_callbackGasLimit;
-    uint16 private constant REQUEST_CONFIRMATIONS = 3;
-    uint32 private constant NUM_WORDS = 1;
+    event RoundOver(string, uint, address payable);
 
-    // Lottery Variables
-    uint256 private immutable i_interval;
-    uint256 private immutable i_entranceFee;
-    uint256 private s_lastTimeStamp;
-    address private s_recentWinner;
-    address payable[] private s_players;
-    RaffleState private s_raffleState;
+    State private lotteryState;
 
-    /* Events */
-    event RequestedRaffleWinner(uint256 indexed requestId);
-    event RaffleEnter(address indexed player);
-    event WinnerPicked(address indexed player);
+    // For randomness
+    event RandomNumberReceived(string message, uint number);
+    VRFCoordinatorV2Interface private COORDINATOR;
 
-    /* Functions */
-    constructor(
-        address vrfCoordinatorV2,//contract
-        uint64 subscriptionId,
-        bytes32 gasLane, // keyHash
-        uint256 interval,
-        uint256 entranceFee,
-        uint32 callbackGasLimit
-    ) VRFConsumerBaseV2(vrfCoordinatorV2) {
-        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
-        i_gasLane = gasLane;
-        i_interval = interval;
-        i_subscriptionId = subscriptionId;
-        i_entranceFee = entranceFee;
-        s_raffleState = RaffleState.OPEN;
-        s_lastTimeStamp = block.timestamp;
-        i_callbackGasLimit = callbackGasLimit;
-    }
-
-    
-    function enterRaffle() public payable {
-        // require(msg.value >= i_entranceFee, "Not enough value sent");
-        // require(s_raffleState == RaffleState.OPEN, "Raffle is not open");
-        if (msg.value < i_entranceFee) {
-            revert Raffle__SendMoreToEnterRaffle();
-        }
-        if (s_raffleState != RaffleState.OPEN) {
-            revert Raffle__RaffleNotOpen();
-        }
-        s_players.push(payable(msg.sender));
-        // Emit an event when we update a dynamic array or mapping
-        // Named events with the function name reversed
-        emit RaffleEnter(msg.sender);
-    }
-
-    /**
-     * @dev This is the function that the Chainlink Keeper nodes call
-     * they look for `upkeepNeeded` to return True.
-     * the following should be true for this to return true:
-     * 1. The time interval has passed between raffle runs.
-     * 2. The lottery is open.
-     * 3. The contract has ETH.
-     * 4. Implicity, your subscription is funded with LINK.
-     */
-    function checkUpkeep(
-        bytes memory /* checkData */
-    )
-        public
-        view
-        override
-        returns (
-            bool upkeepNeeded,
-            bytes memory /* performData */
-        )
+    constructor()
+        VRFConsumerBaseV2(0x7a1BaC17Ccc5b313516C5E16fb24f7659aA5ebed)
     {
-        bool isOpen = RaffleState.OPEN == s_raffleState;
-        bool timePassed = ((block.timestamp - s_lastTimeStamp) > i_interval);
-        bool hasPlayers = s_players.length > 0;
-        bool hasBalance = address(this).balance > 0;
-        upkeepNeeded = (timePassed && isOpen && hasBalance && hasPlayers);
-        return (upkeepNeeded, "0x0"); // can we comment this out?
+        manager = msg.sender;
+        lotteryState = State.STARTED;
+        lotteryId = 1;
+
+        COORDINATOR = VRFCoordinatorV2Interface(
+            0x7a1BaC17Ccc5b313516C5E16fb24f7659aA5ebed
+        ); // VRF Coordinator
     }
 
-    /**
-     * @dev Once `checkUpkeep` is returning `true`, this function is called
-     * and it kicks off a Chainlink VRF call to get a random winner.
-     */
-    //this function replaces requestRandomWords() of the vrfConsumerBase contract
-    function performUpkeep(
-        bytes calldata /* performData */
-    ) external override {
-        (bool upkeepNeeded, ) = checkUpkeep("");
-        // require(upkeepNeeded, "Upkeep not needed");
-        if (!upkeepNeeded) {
-            revert Raffle__UpkeepNotNeeded(
-                address(this).balance,
-                s_players.length,
-                uint256(s_raffleState)
-            );
-        }
-
-        s_raffleState = RaffleState.CALCULATING;
-        uint256 requestId = i_vrfCoordinator.requestRandomWords(
-            i_gasLane,
-            i_subscriptionId,
-            REQUEST_CONFIRMATIONS,
-            i_callbackGasLimit,
-            NUM_WORDS
+    receive() external payable {
+        require(
+            lotteryState == State.STARTED,
+            "Lottery is not yet started. Choose a manager first."
         );
-        // Quiz... is this redundant?
-        emit RequestedRaffleWinner(requestId);
+        require(msg.sender != manager, "Manager cannot participate");
+        require(
+            msg.value == 0.01 * 10**18,
+            "Must send exactly 0.01 ether to participate"
+        );
+
+        if (!alreadyParticipated(msg.sender))
+            participants.push(payable(msg.sender));
     }
 
-    /**
-     * @dev This is the function that Chainlink VRF node
-     * calls to send the money to the random winner.
-     */
+    function getResults() public {
+        require(
+            lotteryState == State.STARTED,
+            "Lottery is not yet started. Choose a manager first."
+        );
+        require(msg.sender == manager, "Only manager can get the results");
+        require(
+            participants.length >= 3,
+            "Lottery must have at least 3 participants"
+        );
+
+        getRandomNumber(); // You may catch the returned request Id
+    }
+
+    function getRandomNumber() private returns (uint requestId) {
+        return
+            COORDINATOR.requestRandomWords(
+                0x4b09e658ed251bcafeebbc69400383d49f344ace09b9576fe248bb02c003fe9f, // keyhash
+                4528, // Subscription Id
+                3, // Request Confirmations
+                2500000, // Callback Gas Limit
+                1 // Number of random numbers to be generated
+            );
+    }
+
+    // This method will be automatically called after getRandomNumber()
     function fulfillRandomWords(
-        uint256, /* requestId */
-        uint256[] memory randomWords
+        uint256,
+        /* requestId */
+        uint256[] memory randomResults
     ) internal override {
-        // s_players size 10
-        // randomNumber % 10  ... this can only generate numbers between 0-9
-    
-        uint256 indexOfWinner = randomWords[0] % s_players.length;
-        address payable recentWinner = s_players[indexOfWinner];
-        s_recentWinner = recentWinner;
-        s_players = new address payable[](0);
-        s_raffleState = RaffleState.OPEN;
-        s_lastTimeStamp = block.timestamp;
-        (bool success, ) = recentWinner.call{value: address(this).balance}("");
-        // require(success, "Transfer failed");
-        if (!success) {
-            revert Raffle__TransferFailed();
+        emit RandomNumberReceived(
+            "Received a truly random number",
+            randomResults[0]
+        );
+
+        // Getting the winner
+        uint randomIndex = randomResults[0] % participants.length;
+
+        address payable winner = participants[randomIndex];
+
+        // Updating the lottery history
+        lotteryHistory[lotteryId] = participants[randomIndex];
+
+        // Resetting the lottery
+        manager = address(0);
+        participants = new address payable[](0);
+        lotteryState = State.ENDED;
+
+        winner.transfer(address(this).balance);
+
+        emit RoundOver(
+            "A lottery round has ended. Following are the winning amount (in ether) and the winner: ",
+            address(this).balance / 10**18,
+            winner
+        );
+    }
+
+    function setNewManager() public {
+        require(
+            lotteryState == State.ENDED,
+            "Cannot change manager in middle of a lottery"
+        );
+        manager = msg.sender;
+        lotteryState = State.STARTED;
+        lotteryId++;
+    }
+
+    function getParticipants() public view returns (address payable[] memory) {
+        return participants;
+    }
+
+    function getCollectedAmount() public view returns (uint) {
+        return address(this).balance;
+    }
+
+    function alreadyParticipated(address participant)
+        private
+        view
+        returns (bool)
+    {
+        bool participated = false;
+
+        // Binary search could be used instead
+        for (uint i = 0; i < participants.length; i++) {
+            if (participants[i] == participant) participated = true;
         }
-        emit WinnerPicked(recentWinner);
-    }
 
-    
-    /** Getter Functions */
-    
-    function getRaffleState() public view returns (RaffleState) {
-        return s_raffleState;
-    }
-    //since NUM_WORDS is constant state variable so the function can be set to pure, it's just like reutrning 1
-    function getNumWords() public pure returns (uint256) {
-        return NUM_WORDS;
-    }
-
-    function getRequestConfirmations() public pure returns (uint256) {
-        return REQUEST_CONFIRMATIONS;
-    }
-
-    function getRecentWinner() public view returns (address) {
-        return s_recentWinner;
-    }
-
-    function getPlayer(uint256 index) public view returns (address) {
-        return s_players[index];
+        return participated;
     }
 }
- //13:59:00 - events and log
-        //whenever we update our dynamic object like an array or mapping, we should always emit an event
-        //EVM has the functionality called a logging
-        //when things happen on a blockchain, the EvM writes these things to a specific data structure called log
-        //we can actually read these logs when connected to a node, we can make a get_logs call to get the log
-        //events allows us to print the information on these logging structure
-        //these events and logs are present in data structures that isn't accessible to smart-contracts
-        //thats why it's cheaper because smart-contracts can't access them
-        //with events we can print some information that is important to us without having to store it in a storage variable which is gonna take up much more gas
-        // each one of these events are tied to the smart-contract or the account address that emitted this event in these
-        //so a transaction happened, an event is emitted, and we can listen for these events
-        // in a chainlink network a node is listening for a request data events for it to get a random numbe, api-call etc
-        //when we emit these events there are 2 kinds of parameters : indexed and non-indexed(encoded with ABI)
-        //we can have upto 3 indexed parameters and these are also known as topics
-        //indexed parameters are much easier to search than the non-indexed ones 
